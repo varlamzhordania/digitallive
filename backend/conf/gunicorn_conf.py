12c282e06eba0c22884ca18710804f2b51419593
Varlam Zhordania <zhordaniavarlam@gmail.com>
2025-05-08 18:52:25 +0400

    Update gunicorn_conf.py

--- backend/conf/gunicorn_conf.py
+++ backend/conf/gunicorn_conf.py
@@ -2,12 +2,12 @@
 bind = '0.0.0.0:8000'
 workers = 5
 timeout = 120
-certfile = '/home/digitallive/cert/cert.crt'
-keyfile = '/home/digitallive/cert/private.key'
+# certfile = '/home/digitallive/cert/cert.crt'
+# keyfile = '/home/digitallive/cert/private.key'
 # Access log - records incoming HTTP requests
-accesslog = "/var/log/gunicorn/gunicorn.access.log"
+# accesslog = "/var/log/gunicorn/gunicorn.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "/var/log/gunicorn/gunicorn.error.log"
+# errorlog = "/var/log/gunicorn/gunicorn.error.log"
 # Whether to send Django output to the error log
 #capture_output = True
 # How verbose the Gunicorn error logs should be
