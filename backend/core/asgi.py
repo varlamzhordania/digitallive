00bba275c5f51db774f5fb69dac7c4d38c0d5965
Varlam Zhordania <zhordaniavarlam@gmail.com>
2025-05-08 16:12:23 +0400

    Update asgi.py

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,25 +1,18 @@
-"""
-ASGI config for core project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from websocket.router import urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
+        # "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(URLRouter(urlpatterns)),
     }
 )
