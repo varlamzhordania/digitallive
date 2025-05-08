785940cf9af7840ef143b0e9cf913ccde738815d
Varlam Zhordania <zhordaniavarlam@gmail.com>
2025-05-08 23:45:38 +0400

    Update forms.py

--- backend/main/forms.py
+++ backend/main/forms.py
@@ -6,7 +6,7 @@ from .models import TickerItem
 class TickerItemForm(forms.ModelForm):
     class Meta:
         model = TickerItem
-        fields = ['ticker', 'content', 'order']
+        fields = ['ticker', 'content', 'order','is_active']
         widgets = {
             'content': CKEditor5Widget(
                 attrs={'class': 'django_ckeditor_5'}, config_name="comment"
